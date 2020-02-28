bc70aa0b38cd7922aefa6ba5ea9215559587cb02
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 06:05:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def map_to_no_change(array)
   final_array=[ ]
   counter = 0 
    while counter < array.size do
-     final_array.push(array[counter] * -1)
+     final_array.push(array[counter])
      counter +=1 
   end
   final_array
