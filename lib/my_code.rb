9607c92221f424f9b486342a225cad635ff2d800
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 06:07:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,11 +33,13 @@ def map_to_double(array)
   final_array=[ ]
   counter = 0 
    while counter < array.size do
-     final_array.push(array[counter]**2)
+     final_array.push(array[counter].square)
      counter +=1 
   end
   final_array
 end
+
+
 
 
 def reduce_to_total(array, starting_point=0)
