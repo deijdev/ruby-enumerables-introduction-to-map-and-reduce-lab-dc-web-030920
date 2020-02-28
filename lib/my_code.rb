f8f019752e3debe5658f2e4b8d5e0532c7fb2846
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 06:17:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,11 +29,11 @@ def map_to_double(array)
   final_array
 end
 
-def map_to_double(array)
+def map_to_square(array)
   final_array=[ ]
   counter = 0 
    while counter < array.size do
-     final_array.push(array[counter]*2)
+     final_array.push(array[counter].square!)
      counter +=1 
   end
   final_array
