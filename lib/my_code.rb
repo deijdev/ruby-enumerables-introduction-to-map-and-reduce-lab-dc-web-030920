b8a439f9d95318c5da81ba93f8e0293921c42390
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 06:03:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,6 +9,16 @@ def map_to_negativize(array)
   final_array
 end
 
+def map_to_no_change(array, starting_point=0)
+  total = starting_point
+  counter = 0 
+  
+  while counter < array.size do
+    total += array[counter]
+    counter += 1 
+  end
+  total
+end
 
 def reduce_to_total(array, starting_point=0)
   total = starting_point
